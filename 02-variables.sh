835a04c318e61b404b1421f92428c973f0890da2
siva3150 <siva123@gmail.com>
2023-12-12 16:22:42 +0530

    first shell script

--- 02-variables.sh
+++ 02-variables.sh
@@ -1,3 +1,15 @@
+# #!/bin/bash
+
+# PERSON1=Ramesh
+# PERSON2=Suresh
+
+
+# echo "$PERSON1: Hello $PERSON2, Good Morning"
+# echo "$PERSON2: Hi $PERSON1, very Good Morning"
+# echo "$PERSON1: How are you $PERSON2?"
+# echo "S$PERSON2: I am good , How are you $PERSON1"
+
+
 #!/bin/bash
 
 PERSON1=Ramesh
