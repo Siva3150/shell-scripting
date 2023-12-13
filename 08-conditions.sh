ca451dd397f5d781f3f789dcc131db3ffdaabad0
siva3150 <siva123@gmail.com>
2023-12-13 20:28:03 +0530

    new commit

--- 08-conditions.sh
+++ 08-conditions.sh
@@ -4,11 +4,7 @@ NUMBER=$1
 
 if [ $NUMBER -gt 100 ]
 then
-
-echo " given $NUMBER  is greather than 100 "
-
+   echo " given $NUMBER  is greather than 100 "
 else
-
-echo " GIVEN $number is less than 100 "
-
+   echo " GIVEN $number is less than 100 "
 fi
