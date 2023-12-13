966f750229c1919b204201ebf491e37da8d1fda5
siva3150 <siva123@gmail.com>
2023-12-13 21:27:51 +0530

    new commit

--- install-mysql.sh
+++ install-mysql.sh
@@ -19,3 +19,13 @@ then
 else
    echo "Installing mysql is success"
 fi
+
+yum install git-y
+
+if [ $? -ne 0 ]
+then
+  echo " ERROR :: Installing git is a failure "
+  exit 1
+else
+  echo " Installing git is a success "
+fi
