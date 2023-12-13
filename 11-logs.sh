d29c9239dd86a70dfb0f6cdd62c77fdd06a37d3c
siva3150 <siva123@gmail.com>
2023-12-13 23:09:29 +0530

    new commit

--- 11-logs.sh
+++ 11-logs.sh
@@ -4,7 +4,14 @@ ID=$(id -u)
 
 TIMESTAMP=$(date +%F-%H-%M-%S)
 
+R=\e[31m
+G=\e[32m
+Y=\e[33m
+N=\e[0m
+
 LOGFILE="/tmp/$0-$TIMESTAMP.log"
+
+echo " Script started executing at $TIMESTAMP " &>> $LOGFILE
 
 echo "script name :: $0"
 
@@ -12,17 +19,17 @@ VALIDATE() {
 
 if [ $1 -ne 0 ]
 then
-   echo "  ERROR :: Installing $2 is failed "
+   echo "  ERROR :: Installing $2 is $R failed "
    exit 1
 else
-   echo "Installing $2 is success"
+   echo "Installing $2 is $G success"
 fi
 
 }
 
 if [ $ID -ne 0 ]
 then
-   echo " ERROR :: please run this script with root access "
+   echo " ERROR :: $R please run this script with root access "
    exit 1
 else
    echo " you are root user "
