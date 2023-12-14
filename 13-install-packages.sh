2d5a8d933cf24a3308f200e4d7c014565e0c6957
siva3150 <siva123@gmail.com>
2023-12-14 17:57:33 +0530

    pushing script

--- 13-install-packages.sh
+++ 13-install-packages.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 ID=(id -u)
-
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
@@ -21,7 +20,7 @@ VALIDATE(){
     fi
 }
 
-if [ "$id" -ne 0 ]
+if [ $ID -ne 0 ]
 then
   echo -e " $R Error :: run this script with root access $N "
   exit 1
